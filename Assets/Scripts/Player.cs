89125218129bbe5b2d27ed41fa628c30c7a0257e
rolyf1 <ro.lyf1mail@gmail.com>
2025-08-16 10:51:55 +0800

    Switching to new InputSystem

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,99 +1,21 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-/*
-    This script provides jumping and movement in Unity 3D - Gatsby
-*/
-
-public class Player : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    // Ground Movement
-    private Rigidbody rb;
-    public float MoveSpeed = 5f;
-    private float moveHorizontal;
-    private float moveForward;
+    public RigidBody3D RigidBody;
+    public float moveSpeed;
+    private Vector3 moveDir;
 
-    // Jumping
-    public float jumpForce = 10f;
-    public float fallMultiplier = 2.5f; // Multiplies gravity when falling down
-    public float ascendMultiplier = 2f; // Multiplies gravity for ascending to peak of jump
-    private bool isGrounded = true;
-    public LayerMask groundLayer;
-    private float groundCheckTimer = 0f;
-    private float groundCheckDelay = 0.3f;
-    private float playerHeight;
-    private float raycastDistance;
+    public InputActionReference move;
 
-    void Start()
+    private void update()
     {
-
+        moveDir = move.action.ReadValue<Vector3>();
     }
 
-    void Update()
+    private void fixUpdate()
     {
-        moveHorizontal = Input.GetAxisRaw("Horizontal");
-        moveForward = Input.GetAxisRaw("Vertical");
-
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            Jump();
-        }
-
-        // Checking when we're on the ground and keeping track of our ground check delay
-        if (!isGrounded && groundCheckTimer <= 0f)
-        {
-            Vector3 rayOrigin = transform.position + Vector3.up * 0.1f;
-            isGrounded = Physics.Raycast(rayOrigin, Vector3.down, raycastDistance, groundLayer);
-        }
-        else
-        {
-            groundCheckTimer -= Time.deltaTime;
-        }
-
-    }
-
-    void FixedUpdate()
-    {
-        MovePlayer();
-        ApplyJumpPhysics();
-    }
-
-    void MovePlayer()
-    {
-
-        Vector3 movement = (transform.right * moveHorizontal + transform.forward * moveForward).normalized;
-        Vector3 targetVelocity = movement * MoveSpeed;
-
-        // Apply movement to the Rigidbody
-        Vector3 velocity = rb.linearVelocity;
-        velocity.x = targetVelocity.x;
-        velocity.z = targetVelocity.z;
-        rb.linearVelocity = velocity;
-
-        // If we aren't moving and are on the ground, stop velocity so we don't slide
-        if (isGrounded && moveHorizontal == 0 && moveForward == 0)
-        {
-            rb.linearVelocity = new Vector3(0, rb.linearVelocity.y, 0);
-        }
-    }
-
-    void Jump()
-    {
-        isGrounded = false;
-        groundCheckTimer = groundCheckDelay;
-        rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z); // Initial burst for the jump
-    }
-
-    void ApplyJumpPhysics()
-    {
-        if (rb.linearVelocity.y < 0) 
-        {
-            // Falling: Apply fall multiplier to make descent faster
-            rb.linearVelocity += Vector3.up * Physics.gravity.y * fallMultiplier * Time.fixedDeltaTime;
-        } // Rising
-        else if (rb.linearVelocity.y > 0)
-        {
-            // Rising: Change multiplier to make player reach peak of jump faster
-            rb.linearVelocity += Vector3.up * Physics.gravity.y * ascendMultiplier  * Time.fixedDeltaTime;
-        }
+        RigidBody.velocity = new Vector3((moveDir.x * moveSpeed), (moveDir.y * moveSpeed), 0);
     }
 }
