9f7a68aa1553a4df2c845b099d6c74e9f23d8906
rolyf1 <ro.lyf1mail@gmail.com>
2025-08-20 10:25:41 +0800

    Small tweaks

--- Assets/Tilt Five/Scripts/TrackPlayer.cs
+++ Assets/Tilt Five/Scripts/TrackPlayer.cs
@@ -15,10 +15,7 @@ namespace TiltFive
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         private void Start()
         {
-            if (PlayerPos != null)
-            {
-                Debug.Log("Object has been paired");
-            }
+            
         }
 
         // Update is called once per frame
